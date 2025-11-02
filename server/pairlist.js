71a4d102d9419df5b933e953f6e3228cfad5403c
infouniverseforloop <info.universefor.loop@gmail.com>
2025-11-02 14:40:55 +0600

    pairlist.js

---
+++ server/pairlist.js
@@ -0,0 +1,70 @@
+// pairsList.js
+// Full final available pairs for Quotex bot (Real, OTC, Crypto, Commodities)
+
+const pairsList = {
+  real: [
+    "EUR/USD",
+    "GBP/USD",
+    "USD/JPY",
+    "AUD/USD",
+    "USD/CAD",
+    "USD/CHF",
+    "NZD/USD"
+  ],
+  otc: [
+    "AUD/NZD",
+    "USD/DZD",
+    "USD/BRL",
+    "GBP/NZD",
+    "USD/JPY",
+    "USD/BDT",
+    "USD/CAD",
+    "USD/CHF",
+    "USD/NGN",
+    "USD/PKR",
+    "USD/PHP",
+    "EUR/GBP",
+    "USD/ZAR",
+    "USD/EGP",
+    "USD/COP",
+    "USD/ARS",
+    "EUR/CAD",
+    "USD/TRY",
+    "NZD/CHF",
+    "AUD/USD",
+    "USD/INR",
+    "CAD/CHF",
+    "AUD/JPY",
+    "AUD/CAD",
+    "EUR/CHF",
+    "USD/IDR"
+  ],
+  crypto: [
+    "Arbitrum",
+    "Dash",
+    "Dogecoin",
+    "Polkadot",
+    "Pepe",
+    "Shiba Inu",
+    "Celestia",
+    "Ripple",
+    "Bitcoin",
+    "Gala",
+    "Binance Coin",
+    "Hamster Kombat",
+    "Toncoin",
+    "Trump",
+    "TRON",
+    "Zcash",
+    "Bonk",
+    "Aptos"
+  ],
+  commodities: [
+    "Gold",
+    "UK Brent",
+    "US Crude",
+    "Silver"
+  ]
+};
+
+module.exports = pairsList;
